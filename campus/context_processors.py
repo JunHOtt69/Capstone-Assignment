3c6c1ecdec2ac6b6bc3ed43bd3ddc08dc6ddf2ff
washingmachine <limjunhong1015@gmail.com>
2026-03-01 00:09:26 +0800

    User card barcode generator

--- campus/context_processors.py
+++ campus/context_processors.py
@@ -1,3 +1,9 @@
+import barcode
+from barcode import get_barcode
+from barcode.writer import ImageWriter
+import io
+import base64
+
 #returning card ID
 def card_context(request):
     u = getattr(request, 'user', None)
@@ -6,9 +12,10 @@ def card_context(request):
     
     role = None
     card_id = None
+    barcode_base64 = None
 
     group_names = set(u.groups.values_list('name', flat=True))
-
+    print('hello1')
     try: 
         if any('admin' in g.lower() for g in group_names) and hasattr(u, 'admin_profile'):
             role = 'admin'
@@ -19,10 +26,23 @@ def card_context(request):
         elif any('student' in g.lower() for g in group_names) and hasattr(u, 'student_profile'):
             role = 'student'
             card_id = u.student_profile.tp_id
-    except Exception:
-        pass
+
+        if card_id:
+            code128 = barcode.get_barcode_class('code128')
+            buffer = io.BytesIO()
+
+            barcode_instance = code128(str(card_id), writer= ImageWriter())
+            barcode_instance.write(buffer)
+
+            base64_str = base64.b64encode(buffer.getvalue()).decode('utf-8')
+            barcode_base64 = f"data:image/png;base64,{base64_str}"
+            print('hello')
+            print('barcode = ', barcode_base64)
+    except Exception as e:
+        print(f"Barcode error: {e}")
     
     return {
         'user_role': role,
-        'card_id' : card_id
+        'card_id' : card_id,
+        'user_barcode' : barcode_base64 if barcode_base64 else None,
     }
