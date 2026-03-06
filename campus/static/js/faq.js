16626909c18aca85fbcd63e5b214dcb43111dcb7
washingmachine <limjunhong1015@gmail.com>
2026-03-06 21:57:31 +0800

    Commit to merge main

--- campus/static/js/faq.js
+++ campus/static/js/faq.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", async function() {
     const quill = new Quill('#editor', {
         theme: 'snow',
         modules: {
