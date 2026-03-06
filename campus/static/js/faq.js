b5811c788ebe9bb6d03c9a705ac74245e4cfba97
washingmachine <limjunhong1015@gmail.com>
2026-03-06 23:53:30 +0800

    Done Comment Section

--- campus/static/js/faq.js
+++ campus/static/js/faq.js
@@ -25,62 +25,118 @@ document.addEventListener("DOMContentLoaded", async function() {
         contentInput.value = html;
     });
 
-    const comments = document.querySelectorAll('.comment');
-    comments.forEach(comment => {
-        const showReplies = comment.querySelector('.showReplies');
-        const repliesContainer = comment.querySelector('.repliesContainer');
 
-        comment.style.setProperty('--last-child', `0vw`);
-        comment.style.setProperty('--extra-len', `1.2vw`);
+    const extraConfig = document.querySelector('.extraConfig');
+    const configContent = document.querySelector('.configContent');
 
-        function addShowBtn(el){
-            const showBtn = el.querySelector('.show');
-            const content = el.querySelector('.commentContent');
+    let idCounter = 1;
 
-            if(showBtn && content.scrollHeight > content.clientHeight){
-                showBtn.classList.add('visible');
-                showBtn.addEventListener('click', (e) => {
-                    e.stopPropagation();
+    function renderComment(){
+        const comments = document.querySelectorAll('.comment');
+        comments.forEach(comment => {
+            const showReplies = comment.querySelector('.showReplies');
+            const repliesContainer = comment.querySelector('.repliesContainer');
+            const replyBtn = comment.querySelector('.reply');
 
-                    if(content.classList.contains('expanded')){
-                        const paragraphs = content.querySelectorAll('p');
+            comment.style.setProperty('--last-child', `0vw`);
+            comment.style.setProperty('--extra-len', `1.2vw`);
 
-                        content.style.display = '-webkit-box'; 
-                        content.style.webkitLineClamp = '4';
+            function addShowBtn(el){
+                const showBtn = el.querySelector('.show');
+                const content = el.querySelector('.commentContent');
 
-                        paragraphs.forEach(p => p.style.display = 'inline');
-                        content.classList.toggle('expanded', false);
-                    }else{
-                        // expanding
-                        const paragraphs = content.querySelectorAll('p');
+                if(showBtn && content.scrollHeight > content.clientHeight){
+                    showBtn.classList.add('visible');
+                    showBtn.addEventListener('click', (e) => {
+                        e.stopPropagation();
 
-                        content.style.display = 'block'; 
-                        content.style.webkitLineClamp = 'none';
+                        if(content.classList.contains('expanded')){
+                            const paragraphs = content.querySelectorAll('p');
 
-                        paragraphs.forEach(p => p.style.display = 'block');
-                        
-                        content.classList.toggle('expanded', true);
-                    }
-                })
-            }
-        }
-        
-        addShowBtn(comment);
-        
-        if(showReplies){
-            showReplies.addEventListener('click', () => {
-                const parent = showReplies.closest('.showMoreReplies');
-                const replies = repliesContainer.querySelectorAll('.comment');
-                if(parent.classList.contains('expanded')){
-                    parent.classList.toggle('expanded', false);
-                }else{
-                    parent.classList.toggle('expanded', true);
-                    
-                    replies.forEach((reply) => {
-                        addShowBtn(reply);
-                    });
+                            content.style.display = '-webkit-box'; 
+                            content.style.webkitLineClamp = '4';
+
+                            paragraphs.forEach(p => p.style.display = 'inline');
+                            content.classList.toggle('expanded', false);
+                        }else{
+                            // expanding
+                            const paragraphs = content.querySelectorAll('p');
+
+                            content.style.display = 'block'; 
+                            content.style.webkitLineClamp = 'none';
+
+                            paragraphs.forEach(p => p.style.display = 'block');
+                            
+                            content.classList.toggle('expanded', true);
+                        }
+                    })
                 }
+            }
+            
+            addShowBtn(comment);
+            
+            if(showReplies){
+                showReplies.addEventListener('click', () => {
+                    const parent = showReplies.closest('.showMoreReplies');
+                    const replies = repliesContainer.querySelectorAll('.comment');
+                    if(parent.classList.contains('expanded')){
+                        parent.classList.toggle('expanded', false);
+                    }else{
+                        parent.classList.toggle('expanded', true);
+                        
+                        replies.forEach((reply) => {
+                            addShowBtn(reply);
+                        });
+                    }
+                });
+            }
+
+            const replyEditors = {};
+
+            replyBtn.addEventListener('click', () => {
+                renderReply(comment, replyEditors);
             });
-        }
-    })
+        })
+    }
+
+    function renderReply(el, dic){
+        const template = document.getElementById('commentInput').content.cloneNode(true);
+        const editor = template.querySelector('.commentEditor');
+        const replyContainer = el.querySelector('.replyContainer');
+        
+        replyContainer.classList.toggle('active', true);
+        replyContainer.innerHTML = ``;
+        
+        const replyBtnWrapper = document.createElement('div');
+        replyBtnWrapper.className = 'replyBtnWrapper';
+        
+        const cancelBtn = document.createElement('button');
+        cancelBtn.className = 'cancel';
+        cancelBtn.innerText = 'Cancel';
+
+        cancelBtn.addEventListener('click', () => {
+            replyContainer.innerHTML = '';
+            replyContainer.classList.toggle('active', false);
+        });
+
+        const replyBtn = document.createElement('button');
+        replyBtn.className = 'reply';
+        replyBtn.innerText = 'Reply';
+
+        idCounter += 1;
+        const editorId = `editor${idCounter}`
+        editor.id = editorId
+        replyContainer.appendChild(editor);
+        replyBtnWrapper.appendChild(cancelBtn);
+        replyBtnWrapper.appendChild(replyBtn);
+        replyContainer.appendChild(replyBtnWrapper);
+
+        dic[editorId] = new Quill(`#${editorId}`, {
+            placeholder: 'Compose an epic...',
+            theme: 'bubble',
+        });
+
+        dic[editorId].focus();
+    }
 }); 
+
